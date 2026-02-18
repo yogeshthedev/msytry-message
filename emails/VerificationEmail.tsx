05f99c6269c29126eebd9af418e54990f7535d9d
Yogesh Meena <developeryogeshh@gmail.com>
2026-02-18 10:57:13 +0530

    feat: implement user signup with email verification
    - Added `VerificationEmail` component for sending verification emails.
    - Created API route for user signup that handles user creation and email verification.
    - Integrated bcrypt for password hashing during user registration.
    - Implemented database connection utility with mongoose.
    - Added resend service for sending emails.
    - Defined ApiResponse type for consistent API responses.
    - Updated package.json to include necessary dependencies for email handling.

---
+++ emails/VerificationEmail.tsx
@@ -0,0 +1,48 @@
+import {
+  Html,  
+  Head,
+  Preview,
+  Heading,
+  Row,
+  Section,
+  Text,
+//   Button,
+} from "@react-email/components";
+
+interface VerificationEmailProps {
+  username: string;
+  otp: string;
+}
+
+export default function VerificationEmail({
+  username,
+  otp,
+}: VerificationEmailProps) {
+  return (
+    <Html lang="en" dir="ltr">
+      <Head>
+        <title>Verification code</title>
+      </Head>
+      <Preview>Here&apos;s your verificaion code: {otp}</Preview>
+      <Section>
+        <Row>
+          <Heading as="h2">Hello {username},</Heading>
+        </Row>
+        <Row>
+          <Text>
+            Thank you for registering. Please use the following OTP to verify
+            your email address:
+          </Text>
+        </Row>
+        <Row>
+          <Text>{otp}</Text>
+        </Row>
+        <Row>
+          <Text>
+            If you did not request this code, please ignore this email.
+          </Text>
+        </Row>
+      </Section>
+    </Html>
+  );
+}
